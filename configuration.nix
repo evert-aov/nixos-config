c925428a9d8c5f335c2533cd606838e16c7b4d9e
Evert <evert.aov@gmail.com>
2026-07-30 10:03:53 -0400

    Clean GNOME, restructure config, add dev tools

--- configuration.nix
+++ configuration.nix
@@ -1,89 +1,177 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 {
-  # Imports
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  # System packages
-  environment.systemPackages = with pkgs; [
-    wget 
-    taskwarrior3
-    inotify-tools
-    lavat
-    file
-    pipes
-    glaxnimate
-    clock-rs
-    cbonsai
-    git
-    killall
-    btop  
-    mpv
-    zenity
-    matugen
-    gpu-screen-recorder
-    neovim 
-    fzf
-    inkscape
-    direnv
-    zbar
-    python3
-    ffmpeg
-    (wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true; }) {})
-    telegram-desktop
-    pkgs.onlyoffice-desktopeditors
-    kitty
-    libreoffice-qt
-    hunspell
-    hunspellDicts.ru_RU
-    hunspellDicts.en_US
-    obsidian
-    obs-studio
-    p7zip
-    papers
-    fastfetch
-    jetbrains.idea
-    quickshell
-    gnome-shell-extensions
-    grim
-    playerctl
-    satty
-    yq-go
-    xdg-desktop-portal-gtk
-    eww
-    swappy
-    slurp
-    mpvpaper
-    gnome-tweaks
-    pkgsCross.mingwW64.stdenv.cc
-    wmctrl
-    bottles
-    qbittorrent
-    power-profiles-daemon
-    jdk8
-    steam-run
+  # ============================================================================
+  # IMPORTS
+  # ============================================================================
+  imports = [
+    ./hardware-configuration.nix
   ];
 
-  environment.pathsToLink = [ "/share/gsettings-schemas" ];
+  # ============================================================================
+  # BOOT & KERNEL
+  # ============================================================================
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
 
-  # User accounts and security
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.cpu.amd.updateMicrocode = true;
+
+  boot.kernelModules = [ "tcp_bbr" ];
+  boot.kernel.sysctl = {
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.core.default_qdisc" = "fq";
+    "net.core.wmem_max" = 1073741824;
+    "net.core.rmem_max" = 1073741824;
+    "net.ipv4.tcp_rmem" = "4096 87380 1073741824";
+    "net.ipv4.tcp_wmem" = "4096 87380 1073741824";
+  };
+
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+    "amd_pstate=active"
+    "tsc=reliable"
+  ];
+
+  boot.plymouth = {
+    enable = true;
+    theme = "simple";
+    themePackages = [
+      (pkgs.stdenv.mkDerivation {
+        pname = "plymouth-theme-simple";
+        version = "1.0";
+        src = ./config/programs/plymouth/simple;
+        installPhase = ''
+          mkdir -p $out/share/plymouth/themes/simple
+          cp -r * $out/share/plymouth/themes/simple/
+          substituteInPlace $out/share/plymouth/themes/simple/simple.plymouth \
+            --replace "@out@" "$out"
+        '';
+      })
+    ];
+  };
+
+  # ============================================================================
+  # NETWORKING
+  # ============================================================================
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = false;
+
+  # ============================================================================
+  # TIME ZONE & LOCALE
+  # ============================================================================
+  time.timeZone = "America/La_Paz";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS     = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY    = "en_US.UTF-8";
+    LC_NAME        = "en_US.UTF-8";
+    LC_NUMERIC     = "en_US.UTF-8";
+    LC_PAPER       = "en_US.UTF-8";
+    LC_TELEPHONE   = "en_US.UTF-8";
+    LC_TIME        = "en_US.UTF-8";
+  };
+
+  # ============================================================================
+  # GRAPHICS & DISPLAY
+  # ============================================================================
+  services.xserver.enable = true;
+  services.displayManager.gdm.enable = true;
+
+  programs.hyprland.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  services.libinput.enable = true;
+  services.udisks2.enable = true;
+
+  # ============================================================================
+  # AUDIO (PipeWire)
+  # ============================================================================
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # ============================================================================
+  # BLUETOOTH
+  # ============================================================================
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # ============================================================================
+  # INPUT (Keyboard)
+  # ============================================================================
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # ============================================================================
+  # SERVICES
+  # ============================================================================
+  services.printing.enable = true;
+  services.openssh.enable = true;
+  services.flatpak.enable = true;
+  services.power-profiles-daemon.enable = true;
+
+  programs.firefox.enable = true;
+  programs.dconf.enable = true;
+
+  services.logind.settings.Login = {
+    HandlePowerKey = "ignore";
+  };
+
+  # ============================================================================
+  # GAMING
+  # ============================================================================
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+  programs.gamemode.enable = true;
+
+  # ============================================================================
+  # SHELL (ZSH)
+  # ============================================================================
+  programs.zsh.enable = true;
+
+  # ============================================================================
+  # USER ACCOUNT
+  # ============================================================================
   users.users.evert = {
     isNormalUser = true;
     description = "evert";
-    extraGroups = [ "networkmanager" "wheel" "video" "adbusers" "libvirtd"]; 
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-    useDefaultShell = true;
     shell = pkgs.zsh;
-  };    
+    extraGroups = [ "networkmanager" "wheel" "video" "adbusers" "libvirtd" ];
+  };
 
   users.defaultUserShell = pkgs.zsh;
   system.userActivationScripts.zshrc = "touch .zshrc";
@@ -100,115 +188,9 @@
     }
   ];
 
-  services.logind.settings.Login = {
-    HandlePowerKey = "ignore";
-  }; 
-  # Program configurations
-  programs.zsh.enable = true;
-
-  # programs.adb no longer needed (systemd 258 handles uaccess rules)
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  programs.dconf = {
-    enable = true;
-  };
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; 
-    dedicatedServer.openFirewall = true; 
-  };
-  programs.gamemode.enable = true;
-
-  # Desktop environment, window managers and theme
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  
-  # Hyprland
-  programs.hyprland.enable = true;
-  
-  # XDG Portals
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Fonts
-  fonts.packages = with pkgs; [
-    udev-gothic-nf
-    noto-fonts
-    liberation_ttf
-  ]; 
-
-  fonts.fontconfig = {
-    enable = true;
-    hinting.style = "slight"; 
-    subpixel.rgba = "rgb"; 
-  };
-
-  # Flatpak
-  services.flatpak.enable = true;
-
-  # Environment Variables
-  # Networking and time
-  networking.hostName = "nixos"; 
-  
-  networking.networkmanager = {
-    enable = true;
-    wifi.powersave = false; 
-  };
-   # Set your time zone.
-  time.timeZone = "America/La_Paz";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Audio and system services
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-  services.blueman.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Power Management Services
-  services.power-profiles-daemon.enable = true; 
-
-  # Nix settings and maintenance
+  # ============================================================================
+  # NIX CONFIGURATION
+  # ============================================================================
   nixpkgs.config.allowUnfree = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -218,74 +200,157 @@
     dates = "daily";
     options = "--delete-older-than 14d";
   };
-  boot = {
-    plymouth = {
-      enable = true;
-      theme = "simple";
-      themePackages = [
-        (pkgs.stdenv.mkDerivation {
-          pname = "plymouth-theme-simple";
-          version = "1.0";
-          
-          # CHANGE THIS to the actual path of your custom theme folder
-          src = ./config/programs/plymouth/simple; 
 
-          installPhase = ''
-            mkdir -p $out/share/plymouth/themes/simple
-            cp -r * $out/share/plymouth/themes/simple/
-            
-            # This dynamically replaces the @out@ placeholder with the real Nix store path
-            substituteInPlace $out/share/plymouth/themes/simple/simple.plymouth \
-              --replace "@out@" "$out"
-          '';
-        })      
-	];
-    };
+  # ============================================================================
+  # NIX-LD (For running generic Linux binaries)
+  # ============================================================================
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    libX11 libXext libXi libXrender libXrandr libXcursor
+    libXinerama libXtst libXScrnSaver libxkbcommon
+    fontconfig freetype gtk3 glib zlib alsa-lib mesa
+    stdenv.cc.cc.lib
+  ];
 
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-      "amd_pstate=active" 
-      "tsc=reliable"
-    ];
-    
-  };
-  # Virtualization
+  # ============================================================================
+  # VIRTUALIZATION
+  # ============================================================================
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
-	
-  # Bootloader and kernel
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  virtualisation.docker.enable = true;
 
-  # Kernel Packages and Optimization
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  hardware.cpu.amd.updateMicrocode = true;
+  # ============================================================================
+  # FONTS
+  # ============================================================================
+  fonts.packages = with pkgs; [
+    udev-gothic-nf
+    noto-fonts
+    liberation_ttf
+  ];
 
-  boot.kernelModules = [ "tcp_bbr" ]; # FIX: Network Congestion Control (Helps with packet jitter)
-  boot.kernel.sysctl = {
-    "net.ipv4.tcp_congestion_control" = "bbr";
-    "net.core.default_qdisc" = "fq";
-    "net.core.wmem_max" = 1073741824;
-    "net.core.rmem_max" = 1073741824;
-    "net.ipv4.tcp_rmem" = "4096 87380 1073741824";
-    "net.ipv4.tcp_wmem" = "4096 87380 1073741824";
+  fonts.fontconfig = {
+    enable = true;
+    hinting.style = "slight";
+    subpixel.rgba = "rgb";
   };
 
-  # FIX: Force CPU to run at max clock speed to prevent frame-time jitter
+  environment.pathsToLink = [ "/share/gsettings-schemas" ];
+
+  # ============================================================================
+  # POWER MANAGEMENT
+  # ============================================================================
   powerManagement.cpuFreqGovernor = "performance";
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
+  # ============================================================================
+  # SYSTEM PACKAGES
+  # ============================================================================
+  environment.systemPackages = with pkgs; [
 
-  system.stateVersion = "25.11"; 
+    # ---- Base & Essentials ----
+    wget
+    git
+    killall
+    fastfetch
+    jq
+    yq-go
+    p7zip
+    neovim
+    fzf
+    fd
+    ripgrep
+    tree
+    zenity
+    file
+    ffmpeg
+    mpv
+
+    # ---- Terminal & Apps ----
+    kitty
+    (wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true; }) {})
+    telegram-desktop
+    obsidian
+    qbittorrent
+    bottles
+
+    # ---- Development ----
+    python3
+    jdk8
+    (jetbrains.idea.override { })
+    jetbrains-toolbox
+
+    # ---- Desktop ----
+    quickshell
+    awww
+    mpvpaper
+    matugen
+    eww
+    grim
+    slurp
+    satty
+    swappy
+    playerctl
+    wl-clipboard
+    cliphist
+    gpu-screen-recorder
+
+    # ---- Customization ----
+    pipes
+    glaxnimate
+    clock-rs
+    cbonsai
+    lavat
+    taskwarrior3
+    inotify-tools
+    direnv
+    zbar
+    hunspell
+    hunspellDicts.ru_RU
+    hunspellDicts.en_US
+    obs-studio
+    inkscape
+    #  rofi
+    networkmanagerapplet
+    xdg-desktop-portal-gtk
+
+    # ---- GTK/Qt Theming ----
+    gtk3
+    # libsForQt5.qt5ct
+    # nwg-look
+
+    # ---- Gaming ----
+    steam-run
+
+    # ---- Cross-compilation ----
+    pkgsCross.mingwW64.stdenv.cc
+    power-profiles-daemon
+    wmctrl
+  ];
+
+  # ============================================================================
+  # FILESYSTEMS (Mounts)
+  # ============================================================================
+  # fileSystems."/mnt/datos" = {
+  #   device = "/dev/disk/by-uuid/bfcecd32-0eef-43df-afe9-3adb96680981";
+  #   fsType = "ext4";
+  #   options = [ "defaults" "noatime" ];
+  # };
+
+  # ============================================================================
+  # DATABASES
+  # ============================================================================
+  # services.postgresql = {
+  #   enable = true;
+  #   ensureDatabases = [ "evert" ];
+  #   ensureUsers = [
+  #     {
+  #       name = "evert";
+  #       ensureDBOwnership = true;
+  #     }
+  #   ];
+  # };
+
+  # ============================================================================
+  # SYSTEM STATE VERSION
+  # ============================================================================
+  system.stateVersion = "25.11";
 }
