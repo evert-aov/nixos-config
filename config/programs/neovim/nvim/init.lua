3404aec3fc24a9ebbf788bb532e85778895f6e9d
Evert <evert.aov@gmail.com>
2026-07-31 23:20:14 -0400

    congigutacion de 2 espacio para tab neovim

--- config/programs/neovim/nvim/init.lua
+++ config/programs/neovim/nvim/init.lua
@@ -17,8 +17,8 @@ vim.opt.termguicolors = true
 vim.opt.signcolumn = 'yes'
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 -- DYNAMIC THEME LOGIC
