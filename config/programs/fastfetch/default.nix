b888551816d421df8b4115fad2209df5b8d43cee
Evert <evert.aov@gmail.com>
2026-08-15 13:33:25 -0400

    feat(fastfetch): auto-symlink fastfetch configs via home-manager

--- config/programs/fastfetch/default.nix
+++ config/programs/fastfetch/default.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{ config, lib, ... }:
 
+let
+  files = builtins.readDir ./.;
+  jsoncFiles = lib.filterAttrs (name: type: type == "regular" && lib.hasSuffix ".jsonc" name) files;
+in
 {
+  xdg.configFile = lib.mapAttrs' (name: _: lib.nameValuePair "fastfetch/${name}" { source = ./${name}; }) jsoncFiles;
 }
