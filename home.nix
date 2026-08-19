da5b9aa29552bc5d21ed55cf497d038dfd31df6b
Evert <evert.aov@gmail.com>
2026-08-19 17:45:49 -0400

    feat(home.nix): add packettracer

--- home.nix
+++ home.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, lib, ... }:
+{ config
+, pkgs
+, lib
+, ...
+}:
 
 let
   # 1. Define the path to your programs directory
@@ -8,66 +12,74 @@ let
   files = builtins.readDir programsDir;
 
   # 3. Filter for directories only (ignoring regular files like .DS_Store or READMEs)
-  directories = builtins.filter 
-    (name: files.${name} == "directory") 
-    (builtins.attrNames files);
+  directories = builtins.filter (name: files.${name} == "directory") (builtins.attrNames files);
 
   # 4. Map the directory names to import paths
   programImports = map (name: programsDir + "/${name}") directories;
 
   # --- Dynamic Scripts Deployment ---
   scriptsDir = ./scripts;
-  scriptFiles = if builtins.pathExists scriptsDir then builtins.readDir scriptsDir else {};
-  scriptFileNames = builtins.filter (name: scriptFiles.${name} == "regular") (builtins.attrNames scriptFiles);
-  scriptHomeFiles = builtins.listToAttrs (map (name: {
-    name = ".local/bin/${lib.removeSuffix ".sh" (lib.removeSuffix ".py" name)}";
-    value = {
-      source = scriptsDir + "/${name}";
-      executable = true;
-    };
-  }) scriptFileNames);
+  scriptFiles = if builtins.pathExists scriptsDir then builtins.readDir scriptsDir else { };
+  scriptFileNames = builtins.filter (name: scriptFiles.${name} == "regular") (
+    builtins.attrNames scriptFiles
+  );
+  scriptHomeFiles = builtins.listToAttrs (
+    map
+      (name: {
+        name = ".local/bin/${lib.removeSuffix ".sh" (lib.removeSuffix ".py" name)}";
+        value = {
+          source = scriptsDir + "/${name}";
+          executable = true;
+        };
+      })
+      scriptFileNames
+  );
 in
 {
   imports = [
     # sessions
     ./config/sessions/hyprland/default.nix
-  ] ++ programImports; 
+  ]
+  ++ programImports;
 
   home.username = "evert";
   home.homeDirectory = "/home/evert";
-  home.stateVersion = "25.11"; 
-  
+  home.stateVersion = "25.11";
+
   home.packages = with pkgs; [
-      adwaita-icon-theme
-      adw-gtk3
-      libsForQt5.qt5ct
-      qt6Packages.qt6ct
-      libsForQt5.qtstyleplugin-kvantum
-      kdePackages.qtstyleplugin-kvantum
-      glow              # preview de markdown en la terminal (yazi)
+    adwaita-icon-theme
+    adw-gtk3
+    libsForQt5.qt5ct
+    qt6Packages.qt6ct
+    libsForQt5.qtstyleplugin-kvantum
+    kdePackages.qtstyleplugin-kvantum
+    glow # preview de markdown en la terminal (yazi)
+    (ciscoPacketTracer8.override {
+      packetTracerSource = /home/evert/Downloads/CiscoPacketTracer_901_Ubuntu_64bit.deb;
+    })
   ];
 
-  # set cursor 
-  home.pointerCursor = 
-  let 
-    getFrom = url: hash: name: {
+  # set cursor
+  home.pointerCursor =
+    let
+      getFrom = url: hash: name: {
         gtk.enable = true;
         x11.enable = true;
         name = name;
         size = 24;
-        package = 
-          pkgs.runCommand "moveUp" {} ''
-            mkdir -p $out/share/icons
-            ln -s ${pkgs.fetchzip {
+        package = pkgs.runCommand "moveUp" { } ''
+          mkdir -p $out/share/icons
+          ln -s ${
+            pkgs.fetchzip {
               url = url;
               hash = hash;
-            }}/dist $out/share/icons/${name}
-          '';
+            }
+          }/dist $out/share/icons/${name}
+        '';
       };
-  in
-    getFrom 
-      "https://github.com/yeyushengfan258/ArcMidnight-Cursors/archive/refs/heads/main.zip"
-      "sha256-VgOpt0rukW0+rSkLFoF9O0xO/qgwieAchAev1vjaqPE=" 
+    in
+    getFrom "https://github.com/yeyushengfan258/ArcMidnight-Cursors/archive/refs/heads/main.zip"
+      "sha256-VgOpt0rukW0+rSkLFoF9O0xO/qgwieAchAev1vjaqPE="
       "ArcMidnight-Cursors";
 
   # Force the dark color scheme and explicitly set GTK3 theme in dconf
@@ -77,7 +89,7 @@ in
       gtk-theme = "adw-gtk3-dark";
     };
   };
-  
+
   home.sessionPath = [
     "$HOME/.local/bin"
     "$HOME/.opencode/bin"
@@ -87,32 +99,32 @@ in
     TERMINAL = "kitty";
   };
 
-  services.easyeffects.enable = true;  
+  services.easyeffects.enable = true;
 
   gtk = {
     enable = true;
-    
-    # IMPORT DYNAMIC MATUGEN COLORS 
+
+    # IMPORT DYNAMIC MATUGEN COLORS
     gtk3.extraCss = ''@import url("file:///home/evert/.cache/matugen/colors-gtk.css");'';
     gtk4.extraCss = ''@import url("file:///home/evert/.cache/matugen/colors-gtk.css");'';
-    
+
     # Target GTK3 specifically
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
       gtk-theme-name = "adw-gtk3-dark";
     };
-    
+
     # Keep GTK4 native but ensure it requests the dark preference
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
   };
-  
+
   qt = {
     enable = true;
     platformTheme.name = "qt6ct";
   };
-  
+
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
@@ -147,8 +159,8 @@ in
 
   programs.home-manager.enable = true;
 
-  fonts.fontconfig.enable = true; 
-  
+  fonts.fontconfig.enable = true;
+
   home.file = {
     ".local/share/fonts/" = {
       source = config/fonts;
@@ -181,5 +193,6 @@ in
         TerminalService=kitty.desktop
       '';
     };
-  } // scriptHomeFiles;
+  }
+  // scriptHomeFiles;
 }
